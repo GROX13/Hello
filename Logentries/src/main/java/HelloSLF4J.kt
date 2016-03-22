0c6b35fda00275b3a0c42727dd65efdd70a40528
GROX13 <giorgirokhadze@gmail.com>
2016-03-22 19:17:48 +0400

    Playing with different libraries.

--- Logentries/src/main/java/HelloSLF4J.kt
+++ Logentries/src/main/java/HelloSLF4J.kt
@@ -5,9 +5,36 @@ object HelloSLF4J {
     private val log = LoggerFactory.getLogger("logentries")
 
     @JvmStatic fun main(args: Array<String>) {
-        
-        
-        log.info("[{\"hello\": \"logger!\"}, {\"hello\": \"logger!\"}, {\"hello\": \"logger!\"}]")
+
+        val validJson =
+                """ 
+                {
+                  "version": "2",
+                  "volume": "blaring",
+                  "current": {
+                               "band": "rednex",
+                               "song": "cotton eye joe",
+                               "members":[ 
+                                         {"firstname":"Kent","lastname":"Olander"}, 
+                                         {"firstname":"Urban","lastname":"Landgren"}, 
+                                         {"firstname":"Jonas","lastname":"Lundstrom"}, 
+                                         {"firstname":"Tor","lastname":"Nilsson"}
+                               ]
+                              }, 
+                  "next": {
+                           "band": "the dubliners",
+                           "song": "finnegan's wake",
+                           "members":[ 
+                                     {"firstname":"Ronnie","lastname":"Drew"}, 
+                                     {"firstname":"Luke","lastname":"Kelly"}, 
+                                     {"firstname":"Ciaran","lastname":"Bourke"}, 
+                                     {"firstname":"Barney","lastname":"McKenna"}
+                           ]
+                        }
+                }
+                """
+
+        log.info(validJson)
 
         System.`in`.read()
     }
